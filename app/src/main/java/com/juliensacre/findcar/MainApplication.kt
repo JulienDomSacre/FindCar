e60a263eb8cdfb728e5f316251e839df8fcc2727
Goldensnake <juliendom.sacre@gmail.com>
2019-03-16 13:32:47 +0100

    update module di

--- app/src/main/java/com/juliensacre/findcar/MainApplication.kt
+++ app/src/main/java/com/juliensacre/findcar/MainApplication.kt
@@ -3,10 +3,14 @@ package com.juliensacre.findcar
 import android.app.Application
 import com.juliensacre.findcar.internal.appModule
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class MainApplication :Application(){
     override fun onCreate() {
         super.onCreate()
+
+        //init log timber
+        Timber.plant(Timber.DebugTree())
 
         // start Koin context
         startKoin(this, listOf(appModule))
--- app/src/main/java/com/juliensacre/findcar/internal/AppModule.kt
+++ app/src/main/java/com/juliensacre/findcar/internal/AppModule.kt
@@ -1,5 +1,18 @@
 package com.juliensacre.findcar.internal
 
+import com.juliensacre.findcar.data.Repository
+import com.juliensacre.findcar.data.RepositoryImpl
+import com.juliensacre.findcar.data.local.CarDatabase
+import com.juliensacre.findcar.data.remote.*
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
 
-val appModule = module {  }
+val appModule = module {
+    single { CarDatabase(androidContext()) }
+    single { get<CarDatabase>().carDao() }
+    single { CarAPIService(get()) }
+
+    single<Repository> { RepositoryImpl(get(), get())}
+    single<ConnectivityInterceptor> { ConnectivityInterceptorImpl(androidContext()) }
+    single<CarDataSource> { CarDataSourceImpl(get()) }
+}
